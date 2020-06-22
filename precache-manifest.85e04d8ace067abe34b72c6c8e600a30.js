269316a44c01e791f1fe71bdefad5703ae00c92b
Andrey Manakhov <andrei_bike@mail.ru>
2020-06-22 23:48:55 +0300

    Add files via upload

---
+++ precache-manifest.85e04d8ace067abe34b72c6c8e600a30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f1b8c29308c3405234b6f2ab12a9c94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d7941cbde26a3b6cd9a",
+    "url": "/static/css/main.36c374bd.chunk.css"
+  },
+  {
+    "revision": "99e65876aa4e11163dc3",
+    "url": "/static/js/2.68c84a23.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.68c84a23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d7941cbde26a3b6cd9a",
+    "url": "/static/js/main.01385404.chunk.js"
+  },
+  {
+    "revision": "660f3ad4819f33f9cc27",
+    "url": "/static/js/runtime-main.4cb3da38.js"
+  },
+  {
+    "revision": "7db702e9735f8b102cc4c7e469eed1ff",
+    "url": "/static/media/preloader.7db702e9.svg"
+  }
+]);
